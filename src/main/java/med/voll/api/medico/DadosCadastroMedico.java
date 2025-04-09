d2359f159862daa4cf876a95b27b93677ae7a599
= <karinac.telles@gmail.com>
2025-04-09 15:50:58 -0300

    excluindo o hello controller porque era só um teste

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,4 +1,6 @@
 package med.voll.api.medico;
 
-public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade) {
+import med.voll.api.endereco.Endereco;
+
+public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, Endereco endereco) {
 }
