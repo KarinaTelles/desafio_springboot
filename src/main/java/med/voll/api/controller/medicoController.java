635e3750e60ef48089a821dbbbc30d46e8c43148
= <karinac.telles@gmail.com>
2025-04-09 15:47:33 -0300

    criando ENUM de especialidades

--- src/main/java/med/voll/api/controller/medicoController.java
+++ src/main/java/med/voll/api/controller/medicoController.java
@@ -1,12 +1,13 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DadosCadastroMedico;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicos")
 public class medicoController {
     @PostMapping
-    public void cadastrar(@RequestBody String json){
+    public void cadastrar(@RequestBody DadosCadastroMedico json){
         System.out.println(json);
     }
 }
