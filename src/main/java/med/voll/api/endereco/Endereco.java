2036f0467952568be5f55135745613bd0f6860b3
= <karinac.telles@gmail.com>
2025-04-11 13:07:35 -0300

    Criando Endereço e Médico

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -1,4 +1,4 @@
 package med.voll.api.endereco;
 
-public record Endereco(String logradouro, String bairro, String cep, String cidade, String complemento, String numero) {
+public record Endereco(String logradouro, String bairro, String cep, String cidade, String complemento, String numero, String uf) {
 }
