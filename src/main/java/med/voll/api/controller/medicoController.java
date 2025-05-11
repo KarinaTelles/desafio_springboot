a9f6dc9036d9d2c6a738ccc6698916babe366b06
= <karinac.telles@gmail.com>
2025-05-11 11:13:48 -0300

    Adicionando uma entidade e um controller

--- src/main/java/med/voll/api/controller/medicoController.java
+++ src/main/java/med/voll/api/controller/medicoController.java
@@ -6,8 +6,14 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/medicos")
 public class medicoController {
+    DadosCadastroMedico ohyeah;
     @PostMapping
     public void cadastrar(@RequestBody DadosCadastroMedico json){
-        System.out.println(json);
+      System.out.println(json);
+      ohyeah = json;
+    }
+    @GetMapping
+    public void imprimeEssaParadaNaTela(){
+        
     }
 }
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -1,13 +1,18 @@
 package med.voll.api.medico;
 
+import jakarta.persistence.*;
 import med.voll.api.endereco.Endereco;
-
+@Table(name="medicos")
+@Entity(name="Medicos")
 public class Medico  {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
     private String email;
     private String crm;
+    @Enumerated(EnumType.STRING)
     private Especialidade especialidade;
+    @Embedded
     private Endereco endereco;
 
 }
