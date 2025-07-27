ef6658fa8dae7df51778c09706c58122333215f9
Karina Telles <karinac.telles@gmail.com>
2025-07-27 18:08:39 -0300

    Nomes mais intuitivos

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,14 +1,14 @@
 package med.voll.api.controller;
 
-import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.CadastroDoMedico;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
-    DadosCadastroMedico dadosCadastroMedico;
+    CadastroDoMedico dadosCadastroMedico;
     @PostMapping
-    public void cadastrar(@RequestBody DadosCadastroMedico json){
+    public void cadastrar(@RequestBody CadastroDoMedico json){
       dadosCadastroMedico = json;
     }
     @GetMapping
