66d33e64625ad665acc2148f9a899bd226d4903e
Karina Telles <karinac.telles@gmail.com>
2025-07-28 11:38:50 -0300

    esqueci a coluna bairro kkkk
    sucesso!

--- src/main/resources/db/migration/V1__create-table-medicos.sql
+++ src/main/resources/db/migration/V1__create-table-medicos.sql
@@ -5,6 +5,7 @@ create table medicos(
     crm varchar(6) not null unique,
     especialidade varchar(100) not null,
     logradouro varchar(100) not null,
+    bairro varchar(100) not null,
     cep varchar(9) not null,
     complemento varchar(100),
     numero varchar(20),
