16bb4c7de52b1241cea5fd61512a13a221dd3910
Karina Telles <karinac.telles@gmail.com>
2025-08-12 20:06:14 -0300

    removendo comentários desnecessário

--- src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
+++ src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
@@ -1,8 +1,5 @@
 package med.voll.api.medico;
 
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import med.voll.api.endereco.DadosEndereco;
 
