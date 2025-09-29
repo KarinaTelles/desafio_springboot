b87fac8a0df2c78b7eccae67ab7450d0d1cefb1a
Karina Telles <karinac.telles@gmail.com>
2025-09-29 17:24:35 -0300

    remoção de comentário desnecessário e delete da classe do Paciente Controller, porque era apenas um rascunho e será implementada a seguir

--- src/main/java/med/api/controller/MedicoController.java
+++ src/main/java/med/api/controller/MedicoController.java
@@ -3,7 +3,6 @@ package med.api.controller;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.api.medico.*;
-import med.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
