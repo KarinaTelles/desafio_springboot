eba4c39b869546905a8f76d974aed6a60a2e712c
Karina Telles <karinac.telles@gmail.com>
2025-10-01 11:43:15 -0300

    atualizando a entidade paciente e criando a migração dela

--- src/main/java/med/api/paciente/Paciente.java
+++ src/main/java/med/api/paciente/Paciente.java
@@ -1,11 +1,13 @@
 package med.api.paciente;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.api.endereco.Endereco;
+import org.hibernate.validator.constraints.br.CPF;
 
 @Table(name="paciente")
 @Entity(name="Paciente")
@@ -17,8 +19,10 @@ public class Paciente {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
+    @Email
     private String email;
     private String telefone;
+    @CPF
     private String cpf;
     @Embedded
     private Endereco endereco;
